8accd0bc7370162155dc083ee9d208b59f9b405c
fhngel1 <45714648+fhngel1@users.noreply.github.com>
2019-02-17 23:26:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,11 @@ const client6 = new Discord.Client();
 const client7 = new Discord.Client();
 const client8 = new Discord.Client();
 const client9 = new Discord.Client();
-const client10 = new Discord.Client();
+
 
 
 client1.on('ready', () => {
-    var x = client1.channels.get("آيدي الروم");
+    var x = client1.channels.get("544270539192139806");
     if (x) x.join();
 
    
@@ -20,53 +20,49 @@ client1.on('ready', () => {
       
 
     client2.on('ready', () => {
-        var x = client2.channels.get("آيدي الروم");
+        var x = client2.channels.get("544239485312040990");
         if (x) x.join();
     
 });
       
 client3.on('ready', () => {
-    var x = client3.channels.get("آيدي الروم");
+    var x = client3.channels.get("544245317370118160");
     if (x) x.join();
 
     });
       
 client4.on('ready', () => {
-    var x = client4.channels.get("آيدي الروم");
+    var x = client4.channels.get("544245519510405161");
     if (x) x.join();
 
     });
       
 client5.on('ready', () => {
-    var x = client5.channels.get("آيدي الروم");
+    var x = client5.channels.get("544245893793185797");
     if (x) x.join();
 
     });
       
 client6.on('ready', () => {
-    var x = client6.channels.get("آيدي الروم");
+    var x = client6.channels.get("544246297675300864");
     if (x) x.join();
 
     });
     client7.on('ready', () => {
-        var x = client7.channels.get("آيدي الروم");
+        var x = client7.channels.get("544246502529433630");
         if (x) x.join();
     
         });
         client8.on('ready', () => {
-            var x = client8.channels.get("آيدي الروم");
+            var x = client8.channels.get("544246786970222606");
             if (x) x.join();
         
             });
             client9.on('ready', () => {
-                var x = client9.channels.get("آيدي الروم");
+                var x = client9.channels.get("545740939763646508");
                 if (x) x.join();
                 });
-                client10.on('ready', () => {
-                    var x = client10.channels.get("آيدي الروم");
-                    if (x) x.join();
-                
-                    });
+              
                     client1.login(process.env.BOT_TOKEN1);   
                     client2.login(process.env.BOT_TOKEN2);   
                     client3.login(process.env.BOT_TOKEN3);   
@@ -76,5 +72,5 @@ client6.on('ready', () => {
                     client7.login(process.env.BOT_TOKEN7);   
                     client8.login(process.env.BOT_TOKEN8);   
                     client9.login(process.env.BOT_TOKEN9);   
-                    client10.login(process.env.BOT_TOKEN10);   
+                
               
